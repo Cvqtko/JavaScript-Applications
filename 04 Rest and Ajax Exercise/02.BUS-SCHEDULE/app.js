a7455621f5181fb6068627d61b4e843c8249234e
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-04 18:26:40 +0200

    Exercises

--- 04 Rest and Ajax Exercise/02.BUS-SCHEDULE/app.js
+++ 04 Rest and Ajax Exercise/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,47 @@
 function solve() {
+	const baseUrl = 'https://judgetests.firebaseio.com/schedule/';
+	let stopId = 'depot';
+	const info = document.getElementById('info');
+	let stopName;
+	const departBtn = document.getElementById('depart');
+	const arriveBtn = document.getElementById('arrive');
+	function changeButton() {
+		if (departBtn.disabled) {
+			departBtn.disabled = false;
+			arriveBtn.disabled = true;
+		} else {
+			departBtn.disabled = true;
+			arriveBtn.disabled = false;
+		}
+	}
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+	function depart() {
+		let url = baseUrl + stopId + '.json';
+		fetch(url).then((response) => response.json())
+			.then(data => {
+				info.textContent = `Next stop ${data.name}`;
+				stopId = data.next;
+				stopName = data.name;
+			})
+			.catch(() => {
+				info.textContent = `Error`;
+				departBtn.disabled = true;
+				arriveBtn.disabled = true;
+				return;
+			});
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+		changeButton();
+	}
 
-    return {
-        depart,
-        arrive
-    };
+	function arrive() {
+		info.textContent = `Arriving at ${stopName}`;
+		changeButton();
+	}
+
+	return {
+		depart,
+		arrive
+	};
 }
 
 let result = solve();
