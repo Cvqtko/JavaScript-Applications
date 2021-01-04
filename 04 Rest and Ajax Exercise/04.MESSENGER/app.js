eb8a946445e8c0a329a9e15dcd01fdd0c2e9168c
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-04 20:19:46 +0200

    Exercises

--- 04 Rest and Ajax Exercise/04.MESSENGER/app.js
+++ 04 Rest and Ajax Exercise/04.MESSENGER/app.js
@@ -1,5 +1,33 @@
 function attachEvents() {
-    console.log('TODO...');
+	let url = "https://rest-messanger.firebaseio.com/messanger.json";
+
+	let refreshBtn = document.getElementById("refresh");
+	let submitBtn = document.getElementById("submit");
+	refreshBtn.addEventListener('click', () => {
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				let result = Object.values(data).reduce((messages, message) =>
+					(messages += `${message.author}: ${message.content} \n`), '');
+				document.getElementById('messages').textContent = result;
+			});
+
+
+	});
+
+	submitBtn.addEventListener('click', () => {
+		let author = document.getElementById("author");
+		let content = document.getElementById("content");
+		fetch(url, {
+			method: 'POST', body: JSON.stringify({
+				author: author.value,
+				content: content.value
+			})
+			
+		});
+		author.value = '';
+		content.value = '';
+	});
 }
 
 attachEvents();
